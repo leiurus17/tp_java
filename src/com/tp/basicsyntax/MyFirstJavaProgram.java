32f753a86239c699755be2ef2f540f29f1a396b2
Marlon <martianworm17@gmail.com>
2017-09-29 23:15:40 +0800

    Java - Basic Syntax - Comments in Java

--- src/com/tp/basicsyntax/MyFirstJavaProgram.java
+++ src/com/tp/basicsyntax/MyFirstJavaProgram.java
@@ -4,9 +4,12 @@ public class MyFirstJavaProgram {
 
 	/* This is my first Java program.
 	 * This will print 'Hello World' as the output
+	 * This is an example of multi-line comments.
 	 */
 	
 	public static void main(String []args) {
+		// This is an example of single line comment
+		/* This is also an example of single line comment. */
 		System.out.println("Hello World"); // prints Hello World
 	}
 }
