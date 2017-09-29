aa080bac4ba0eb3a3f1ab4bf36dc36462c52ebe7
Marlon <martianworm17@gmail.com>
2017-09-29 23:09:33 +0800

    Java - Basic Syntax

---
+++ src/com/tp/basicsyntax/MyFirstJavaProgram.java
@@ -0,0 +1,12 @@
+package com.tp.basicsyntax;
+
+public class MyFirstJavaProgram {
+
+	/* This is my first Java program.
+	 * This will print 'Hello World' as the output
+	 */
+	
+	public static void main(String []args) {
+		System.out.println("Hello World"); // prints Hello World
+	}
+}
