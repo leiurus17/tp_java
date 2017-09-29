d3645e6ea52078dbdea9a2595697ced34bc2438a
Marlon <martianworm17@gmail.com>
2017-09-29 23:12:53 +0800

    Java - Basic Syntax

---
+++ src/com/tp/basicsyntax/FreshJuiceTest.java
@@ -0,0 +1,16 @@
+package com.tp.basicsyntax;
+
+class FreshJuice {
+	enum FreshJuiceSize{ SMALL, MEDIUM, LARGE }
+	FreshJuiceSize size;
+}
+
+public class FreshJuiceTest {
+	
+	public static void main(String args[]) {
+		FreshJuice juice = new FreshJuice();
+		juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
+		
+		System.out.println("Size: " + juice.size);
+	}
+}
