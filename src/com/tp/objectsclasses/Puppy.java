970ba3c0bee87ce7fa0ff943aed79fb96eddf587
Marlon <martianworm17@gmail.com>
2017-10-16 21:25:50 +0800

    Accessing Instance Variables and Methods

--- src/com/tp/objectsclasses/Puppy.java
+++ src/com/tp/objectsclasses/Puppy.java
@@ -1,6 +1,7 @@
 package com.tp.objectsclasses;
 
 public class Puppy {
+	int puppyAge;
 
 	public Puppy() {
 		// Default constructor but not so default because I defined it
@@ -11,9 +12,28 @@ public class Puppy {
 		System.out.println("Passed name is: " + name);
 	}
 	
+	public void setAge(int age) {
+		puppyAge = age;
+	}
+	
+	public int getAge() {
+		System.out.println("Puppy's age is : " + puppyAge);
+		return puppyAge;
+	}
+	
 	public static void main (String[] args) {
 		// Following statement would create an object myPuppy
 		// And it will print out the Passed name is even if it is just instantiated
+		/* Object creation */
 		Puppy myPuppy = new Puppy("tommy");
+		
+		/* Call class method to set puppy's age */
+		myPuppy.setAge(2);
+		
+		/* Call another class method to get puppy's age */
+		myPuppy.getAge();
+		
+		/* You can access instance variables as follows as well */
+		System.out.println("Variable Value: " + myPuppy.puppyAge);
 	}
 }
