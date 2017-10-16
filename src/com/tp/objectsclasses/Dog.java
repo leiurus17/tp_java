05db5d4e1e31ce06558de5ad73a5050dbf24631b
Marlon <martianworm17@gmail.com>
2017-10-16 17:35:26 +0800

    Sample of a class. Dog.

---
+++ src/com/tp/objectsclasses/Dog.java
@@ -0,0 +1,23 @@
+package com.tp.objectsclasses;
+
+public class Dog {
+	String breed;
+	String color;
+	int age;
+	
+	void barking() {
+		
+	}
+	
+	void hungry() {
+		
+	}
+	
+	void sleeping() {
+		
+	}
+	
+	void running() {
+		
+	}
+}
