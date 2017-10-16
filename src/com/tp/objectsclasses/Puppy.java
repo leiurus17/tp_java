5a52e0f1ce04bdb01f02396f3551f9b6c7f8de53
Marlon <martianworm17@gmail.com>
2017-10-16 18:45:11 +0800

    Puppy class with constructors

---
+++ src/com/tp/objectsclasses/Puppy.java
@@ -0,0 +1,12 @@
+package com.tp.objectsclasses;
+
+public class Puppy {
+
+	public Puppy() {
+		// Default constructor but not so default because I defined it
+	}
+	
+	public Puppy(String name) {
+		// This constructor has one parameter, name.
+	}
+}
