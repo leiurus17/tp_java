0959a08e2de1e5a4967733a89709773bb35c9876
Marlon <martianworm17@gmail.com>
2017-10-16 19:04:09 +0800

    What?

--- src/com/tp/objectsclasses/singleton/SingletonDemo.java
+++ src/com/tp/objectsclasses/singleton/SingletonDemo.java
@@ -4,6 +4,8 @@ public class SingletonDemo {
 
 	public static void main(String[] args) {
 		Singleton tmp = Singleton.getInstance();
+		Singleton tmp2 = Singleton.getInstance();
 		tmp.demoMethod();
+		tmp2.demoMethod();
 	}
 }
