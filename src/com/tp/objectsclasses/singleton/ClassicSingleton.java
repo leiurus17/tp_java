dfed71589efbd2678078a63da76d54ba66ec8b11
Marlon <martianworm17@gmail.com>
2017-10-16 19:07:37 +0800

    Classic Singleton design pattern

---
+++ src/com/tp/objectsclasses/singleton/ClassicSingleton.java
@@ -0,0 +1,17 @@
+package com.tp.objectsclasses.singleton;
+
+public class ClassicSingleton {
+
+	private static ClassicSingleton instance = null;
+	private ClassicSingleton() {
+		// Exists only to defeat instantiation.
+	}
+	
+	public static ClassicSingleton getInstance() {
+		if(instance == null) {
+			instance = new ClassicSingleton();
+		}
+		return instance;
+	}
+	
+}
