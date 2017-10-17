73046b02de443d5467d51ba03b2f2aa3162b5342
Marlon <martianworm17@gmail.com>
2017-10-17 22:29:07 +0800

    Instance Variables

---
+++ src/com/tp/variabletypes/Employee.java
@@ -0,0 +1,33 @@
+package com.tp.variabletypes;
+
+public class Employee {
+
+	// This instance variable is visible for any child class.
+	public String name;
+	
+	// salary variable is visible in Employee class only.
+	private double salary;
+	
+	// The name variable is assigned in the constructor.
+	public Employee (String empName) {
+		name = empName;
+	}
+	
+	// The salary variable is assigned a value.
+	public void setSalary(double empSal) {
+		salary = empSal;
+	}
+	
+	// This method prints the employee details.
+	public void printEmp() {
+		System.out.println("name : " + name);
+		System.out.println("salary : " + salary);
+	}
+	
+	public static void main(String args[]) {
+		Employee empOne = new Employee("Indra");
+		
+		empOne.setSalary(1000);
+		empOne.printEmp();
+	}
+}
