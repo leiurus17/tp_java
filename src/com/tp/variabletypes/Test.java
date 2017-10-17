603a5ed54ef862fff2c2df6cd47de3be60618957
Marlon <martianworm17@gmail.com>
2017-10-17 22:18:46 +0800

    Local variables should be initialized

--- src/com/tp/variabletypes/Test.java
+++ src/com/tp/variabletypes/Test.java
@@ -3,6 +3,7 @@ package com.tp.variabletypes;
 public class Test {
 
 	public void pupAge() {
+		// int age; // The local variable age may not have been initialized
 		int age = 0;
 		age = age + 7;
 		System.out.println("Puppy age is : " + age);
