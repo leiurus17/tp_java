ee8ed7d5273c082260c4884281a5faf55f54298e
Marlon <martianworm17@gmail.com>
2017-10-17 22:15:42 +0800

    Local Variables

---
+++ src/com/tp/variabletypes/Test.java
@@ -0,0 +1,16 @@
+package com.tp.variabletypes;
+
+public class Test {
+
+	public void pupAge() {
+		int age = 0;
+		age = age + 7;
+		System.out.println("Puppy age is : " + age);
+	}
+	
+	public static void main(String args[]) {
+		Test test = new Test();
+		test.pupAge();
+	}
+	
+}
