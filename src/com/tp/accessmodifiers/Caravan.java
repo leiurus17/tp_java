3bf8dce30dbdd7ef603e889bfcd634188e605479
Marlon <martianworm17@gmail.com>
2017-10-18 20:35:10 +0800

    Abstract Class

---
+++ src/com/tp/accessmodifiers/Caravan.java
@@ -0,0 +1,10 @@
+package com.tp.accessmodifiers;
+
+abstract class Caravan {
+	private double price;
+	private String model;
+	private String year;
+	public abstract void goFast(); // an abstract method
+	public abstract void changeColor();
+	
+}
