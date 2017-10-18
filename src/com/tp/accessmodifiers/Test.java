44856fb9c7ed1cd9d1858bca93648d010377ae24
Marlon <martianworm17@gmail.com>
2017-10-18 19:00:46 +0800

    The Final Modifier

---
+++ src/com/tp/accessmodifiers/Test.java
@@ -0,0 +1,14 @@
+package com.tp.accessmodifiers;
+
+public class Test {
+
+	final int value = 10;
+	
+	// The following are examples of declaring constants:
+	public static final int BOXWIDTH = 6;
+	static final String TITLE = "Manager";
+	
+	public void changeValue() {
+		 // value = 12;
+	}
+}
