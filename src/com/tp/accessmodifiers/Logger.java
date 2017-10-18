8121f7757b292aebea26f7dbbd9f3d47d19303a9
Marlon <martianworm17@gmail.com>
2017-10-18 18:06:35 +0800

    Private Access Modifier - Private

---
+++ src/com/tp/accessmodifiers/Logger.java
@@ -0,0 +1,15 @@
+package com.tp.accessmodifiers;
+
+public class Logger {
+	
+	private String format;
+	
+	public String getFormat() {
+		return this.format;
+	}
+	
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+}
