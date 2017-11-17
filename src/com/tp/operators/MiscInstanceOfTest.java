e447a4a71cad6e139a84317cd415bbc1a8981167
Marlon <martianworm17@gmail.com>
2017-11-17 21:52:06 +0800

    instanceof Operator

---
+++ src/com/tp/operators/MiscInstanceOfTest.java
@@ -0,0 +1,15 @@
+package com.tp.operators;
+
+public class MiscInstanceOfTest {
+
+	public static void main(String[] args) {
+		
+		String name = "James";
+		
+		// Following will return true since name is type of String
+		boolean result = name instanceof String;
+		System.out.println(result);
+			
+		
+	}
+}
