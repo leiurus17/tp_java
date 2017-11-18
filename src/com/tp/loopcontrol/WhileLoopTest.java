c5e3da22b3357d9293a501e70e0f63ce76ae7a89
Marlon <martianworm17@gmail.com>
2017-11-18 09:09:12 +0800

    while Loop in java

---
+++ src/com/tp/loopcontrol/WhileLoopTest.java
@@ -0,0 +1,15 @@
+package com.tp.loopcontrol;
+
+public class WhileLoopTest {
+
+	public static void main(String[] args) {
+		int x = 10;
+		
+		while(x < 20) {
+			System.out.print("Value of x : " + x);
+			x++;
+			System.out.println();
+		}
+	}
+	
+}
