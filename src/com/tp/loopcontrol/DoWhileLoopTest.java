6dca24e5bfed075e2654652c7d5dbec29661450e
Marlon <martianworm17@gmail.com>
2017-11-19 18:27:31 +0800

    do while loop in java

---
+++ src/com/tp/loopcontrol/DoWhileLoopTest.java
@@ -0,0 +1,14 @@
+package com.tp.loopcontrol;
+
+public class DoWhileLoopTest {
+
+	public static void main(String[] args) {
+		int x = 10;
+		
+		do {
+			System.out.print("Value of x : " + x);
+			x++;
+			System.out.println();
+		} while(x < 20);
+	}
+}
