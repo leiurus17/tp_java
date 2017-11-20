8813003cba23a0889c91fe9fdcb82249e2a2113b
Marlon <martianworm17@gmail.com>
2017-11-20 20:53:10 +0800

    Break statement in Java

---
+++ src/com/tp/loopcontrol/BreakLoopTest.java
@@ -0,0 +1,15 @@
+package com.tp.loopcontrol;
+
+public class BreakLoopTest {
+	
+	public static void main(String[] args) {
+		int [] numbers = {10, 20, 30, 40, 50};
+		
+		for(int x: numbers) {
+			if(x == 30) {
+				break;
+			}
+			System.out.println(x);
+		}
+	}
+}
