d3f480d5270831565106ed8df80910757faab389
Marlon <martianworm17@gmail.com>
2017-11-20 20:59:10 +0800

    Continue statement in Java

---
+++ src/com/tp/loopcontrol/ContinueLoopTest.java
@@ -0,0 +1,14 @@
+package com.tp.loopcontrol;
+
+public class ContinueLoopTest {
+	public static void main(String[] args) {
+		int [] numbers = {10, 20, 30, 40, 50};
+		
+		for(int x: numbers) {
+			if(x == 30) {
+				continue;
+			}
+			System.out.println(x);
+		}
+	}
+}
