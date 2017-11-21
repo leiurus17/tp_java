14fff41c8c5dabfead457a51aa33a9a5bc9bfb64
Marlon <martianworm17@gmail.com>
2017-11-21 21:29:20 +0800

    IF statement in Java

---
+++ src/com/tp/decisionmaking/IfStatementTest.java
@@ -0,0 +1,12 @@
+package com.tp.decisionmaking;
+
+public class IfStatementTest {
+
+	public static void main(String[] args) {
+		int x = 10;
+		
+		if(x < 20) {
+			System.out.println("This is if statement.");
+		}
+	}
+}
