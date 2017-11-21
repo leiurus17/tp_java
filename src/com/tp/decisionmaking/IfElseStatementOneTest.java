c93e42e57d992add06561950baa6e82c98f94ef1
Marlon <martianworm17@gmail.com>
2017-11-21 21:33:55 +0800

    IF-ELSE statement in Java

---
+++ src/com/tp/decisionmaking/IfElseStatementOneTest.java
@@ -0,0 +1,14 @@
+package com.tp.decisionmaking;
+
+public class IfElseStatementOneTest {
+
+	public static void main(String[] args) {
+		int x = 30;
+		
+		if(x < 20) {
+			System.out.println("This is if statement.");
+		} else {
+			System.out.println("This is else statement.");
+		}
+	}
+}
