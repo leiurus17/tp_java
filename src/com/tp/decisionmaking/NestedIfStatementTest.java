7bdbd793fd9fa7449770c0e33ef1a7a5fe164d9f
Marlon <martianworm17@gmail.com>
2017-11-22 21:14:07 +0800

    Nested IF statement in Java

---
+++ src/com/tp/decisionmaking/NestedIfStatementTest.java
@@ -0,0 +1,14 @@
+package com.tp.decisionmaking;
+
+public class NestedIfStatementTest {
+	public static void main(String[] args) {
+		int x = 30;
+		int y = 10;
+		
+		if(x == 30) {
+			if (y == 10) {
+				System.out.println("X = 30 and Y = 10");
+			}
+		}
+	}
+}
