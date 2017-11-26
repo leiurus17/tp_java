e7ba61b27a42cec5b01979b7265c6119df067dd5
Marlon <martianworm17@gmail.com>
2017-11-26 11:55:03 +0800

    Java - abs() Method

---
+++ src/com/tp/numbers/NumberAbsTest.java
@@ -0,0 +1,15 @@
+package com.tp.numbers;
+
+public class NumberAbsTest {
+	
+	public static void main(String[] args) {
+		Integer a = -8;
+		double d  = -100;
+		float f   = -90;
+		
+		System.out.println(Math.abs(a));
+		System.out.println(Math.abs(d));
+		System.out.println(Math.abs(f));
+	}
+
+}
