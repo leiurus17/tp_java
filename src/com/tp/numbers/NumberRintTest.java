347634bc6f0d0e643a309e5cbee4c323b3be7ab8
Marlon <martianworm17@gmail.com>
2017-11-26 14:01:12 +0800

    Java - rint() Method

---
+++ src/com/tp/numbers/NumberRintTest.java
@@ -0,0 +1,16 @@
+package com.tp.numbers;
+
+public class NumberRintTest {
+
+	public static void main(String[] args) {
+		double d = 100.675;
+		double e = 100.500;
+		double f = 100.200;
+		double g = 100.501;
+		
+		System.out.println(Math.rint(d));
+		System.out.println(Math.rint(e));
+		System.out.println(Math.rint(f));
+		System.out.println(Math.rint(g));
+	}
+}
