827cd4d69e13906f16f249d44c57fbd472792f4a
Marlon <martianworm17@gmail.com>
2017-11-26 12:04:09 +0800

    Java - ceil() Method

---
+++ src/com/tp/numbers/NumberCeilTest.java
@@ -0,0 +1,15 @@
+package com.tp.numbers;
+
+public class NumberCeilTest {
+
+	public static void main(String[] args) {
+		double d = -100.675;
+		float  f = -90;
+		
+		System.out.println(Math.ceil(d));
+		System.out.println(Math.ceil(f));
+		
+		System.out.println(Math.floor(d));
+		System.out.println(Math.floor(f));
+	}
+}
