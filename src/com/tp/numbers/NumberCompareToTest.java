7717f070341d891b15f72441ac9b988b63632929
Marlon <martianworm17@gmail.com>
2017-11-26 10:36:42 +0800

    Java - compareTo() Method

---
+++ src/com/tp/numbers/NumberCompareToTest.java
@@ -0,0 +1,12 @@
+package com.tp.numbers;
+
+public class NumberCompareToTest {
+
+	public static void main(String[] args) {
+		Integer x = 5;
+		
+		System.out.println(x.compareTo(3));
+		System.out.println(x.compareTo(5));
+		System.out.println(x.compareTo(8));
+	}
+}
