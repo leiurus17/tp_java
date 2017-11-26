3fd59d3683bf3352a0f04939ce56d7d204850a3a
Marlon <martianworm17@gmail.com>
2017-11-26 11:27:43 +0800

    Java - toString() Method

---
+++ src/com/tp/numbers/NumberToStringTest.java
@@ -0,0 +1,12 @@
+package com.tp.numbers;
+
+public class NumberToStringTest {
+
+	public static void main(String[] args) {
+		Integer x = 5;
+		
+		System.out.println(x.toString());
+		System.out.println(Integer.toString(12));
+		System.out.println(x.toString() + Integer.toString(12));
+	}
+}
