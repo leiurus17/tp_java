e00aa6140f2cc1baf6b7485b14f6f0a6c8d31ead
Marlon <martianworm17@gmail.com>
2017-11-26 10:21:29 +0800

    Java - xxxValue() Method

---
+++ src/com/tp/numbers/PrimitiveValueTest.java
@@ -0,0 +1,17 @@
+package com.tp.numbers;
+
+public class PrimitiveValueTest {
+
+	public static void main(String[] args) {
+		Integer x = 5;
+		
+		// Returns byte primitive data type
+		System.out.println(x.byteValue());
+		
+		// Returns double primitive data type
+		System.out.println(x.doubleValue());
+		
+		// Returns long primitive data type
+		System.out.println(x.longValue());
+	}
+}
