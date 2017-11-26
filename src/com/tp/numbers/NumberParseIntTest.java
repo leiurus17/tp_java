219bbf77a8058a1054cadf6bdfd632ad9664f0e9
Marlon <martianworm17@gmail.com>
2017-11-26 11:50:38 +0800

    Java - parseInt() Method

---
+++ src/com/tp/numbers/NumberParseIntTest.java
@@ -0,0 +1,14 @@
+package com.tp.numbers;
+
+public class NumberParseIntTest {
+
+	public static void main(String[] args) {
+		int x = Integer.parseInt("9");
+		double c = Double.parseDouble("5");
+		int b = Integer.parseInt("444", 16);
+		
+		System.out.println(x);
+		System.out.println(c);
+		System.out.println(b);
+	}
+}
