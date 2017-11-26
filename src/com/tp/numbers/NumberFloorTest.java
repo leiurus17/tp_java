003f0e1bc7177ce459f1cec02331e0298b967419
Marlon <martianworm17@gmail.com>
2017-11-26 12:08:51 +0800

    Java - floor() Method

---
+++ src/com/tp/numbers/NumberFloorTest.java
@@ -0,0 +1,15 @@
+package com.tp.numbers;
+
+public class NumberFloorTest {
+
+	public static void maing(String[] args) {
+		double d = -100.675;
+		float  f = -90;
+		
+		System.out.println(Math.floor(d));
+		System.out.println(Math.floor(f));
+		
+		System.out.println(Math.ceil(d));
+		System.out.println(Math.ceil(f));
+	}
+}
