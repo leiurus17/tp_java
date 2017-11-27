cb76fadaec2c9f2c693ed49633387faf7933ee5d
Marlon <martianworm17@gmail.com>
2017-11-27 20:55:41 +0800

    Java - max() Method

---
+++ src/com/tp/numbers/NumberMaxTest.java
@@ -0,0 +1,10 @@
+package com.tp.numbers;
+
+public class NumberMaxTest {
+	
+	public static void main(String[] args) {
+		System.out.println(Math.max(12.123, 12.456));
+		System.out.println(Math.max(23.12, 23.0));
+	}
+
+}
