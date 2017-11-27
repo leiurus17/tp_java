10cfc2dc373a0aaaa10c3e38e3a4d57a10511f9c
Marlon <martianworm17@gmail.com>
2017-11-27 21:11:28 +0800

    Java - log() Method

---
+++ src/com/tp/numbers/NumberLogTest.java
@@ -0,0 +1,11 @@
+package com.tp.numbers;
+
+public class NumberLogTest {
+
+	public static void main(String[] args) {
+		double x = 11.635;
+		
+		System.out.printf("The value of e is %.4f%n", Math.E);
+		System.out.printf("log(%.3f) is %.3f%n", x, Math.log(x));
+	}
+}
