21cc4a1e1f2781014fb6eb0e0d608b3873a26410
Marlon <martianworm17@gmail.com>
2017-11-27 21:04:58 +0800

    Java - exp() Method

---
+++ src/com/tp/numbers/NumberExpTest.java
@@ -0,0 +1,11 @@
+package com.tp.numbers;
+
+public class NumberExpTest {
+
+	public static void main(String[] args) {
+		double x = 11.635;
+		
+		System.out.printf("The value of e is %.4f%n", Math.E);
+		System.out.printf("exp(%.3f) is %.3f%n", x, Math.exp(x));
+	}
+}
