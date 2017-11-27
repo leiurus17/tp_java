f32af6c628dacddbb049c60042b1aac917ad137b
Marlon <martianworm17@gmail.com>
2017-11-27 20:54:19 +0800

    Java - min() Method

---
+++ src/com/tp/numbers/NumberMinTest.java
@@ -0,0 +1,10 @@
+package com.tp.numbers;
+
+public class NumberMinTest {
+	
+	public static void main(String[] args) {
+		System.out.println(Math.min(12.123, 12.456));
+		System.out.println(Math.min(23.12, 23.0));
+	}
+
+}
