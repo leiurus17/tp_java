7b88de90e737d2fb7e5c60c7131544d090f49b43
Marlon <martianworm17@gmail.com>
2017-11-27 20:37:35 +0800

    Java - round() Method

---
+++ src/com/tp/numbers/NumberRoundTest.java
@@ -0,0 +1,17 @@
+package com.tp.numbers;
+
+public class NumberRoundTest {
+	
+	public static void main(String[] args) {
+		double d = 100.675;
+		double e = 100.500;
+		float f = 100;
+		float g = 90f;
+		
+		System.out.println(Math.round(d));
+		System.out.println(Math.round(e));
+		System.out.println(Math.round(f));
+		System.out.println(Math.round(g));
+	}
+
+}
