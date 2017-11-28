4bc998f500f3c6df004108bc4c60564d894be921
Marlon <martianworm17@gmail.com>
2017-11-28 15:10:03 +0800

    Java - pow() Method

---
+++ src/com/tp/numbers/NumberPowTest.java
@@ -0,0 +1,13 @@
+package com.tp.numbers;
+
+public class NumberPowTest {
+	
+	public static void main(String[] args) {
+		double x = 11.635;
+		double y = 2.76;
+		
+		System.out.printf("The value of e is %.4f%n", Math.E);
+		System.out.printf("exp(%.3f) is %.3f%n", x, Math.pow(x, y));
+	}
+
+}
