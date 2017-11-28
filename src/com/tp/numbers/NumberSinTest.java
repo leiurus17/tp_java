e0cd2c21ab8b09da8d55a548539d4a6483ed604b
Marlon <martianworm17@gmail.com>
2017-11-28 15:16:17 +0800

    Java - sin() Method

---
+++ src/com/tp/numbers/NumberSinTest.java
@@ -0,0 +1,13 @@
+package com.tp.numbers;
+
+public class NumberSinTest {
+	
+	public static void main(String[] args) {
+		double degrees = 45.0;
+		double radians = Math.toRadians(degrees);
+		
+		System.out.format("The value of pi is %.4f%n", Math.PI);
+		System.out.format("The sine of %.1f degrees is %.4f%n", degrees, Math.sin(radians));
+	}
+
+}
