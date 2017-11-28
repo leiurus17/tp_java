600dc7333f9b728fa6180ce36d1d51b23a6c4564
Marlon <martianworm17@gmail.com>
2017-11-28 15:46:42 +0800

    Java - toRadians() Method

---
+++ src/com/tp/numbers/NumberToRadiansTest.java
@@ -0,0 +1,12 @@
+package com.tp.numbers;
+
+public class NumberToRadiansTest {
+
+	public static void main(String[] args) {
+		double x = 45.0;
+		double y = 30.0;
+		
+		System.out.println(Math.toRadians(x));
+		System.out.println(Math.toRadians(y));
+	}
+}
