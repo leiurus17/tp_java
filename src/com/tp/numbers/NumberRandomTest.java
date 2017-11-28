7e23e51566dc8ca8a65634ad3b946a46e8973eca
Marlon <martianworm17@gmail.com>
2017-11-28 15:50:45 +0800

    Java - random() Method

---
+++ src/com/tp/numbers/NumberRandomTest.java
@@ -0,0 +1,11 @@
+package com.tp.numbers;
+
+public class NumberRandomTest {
+
+	public static void main(String[] args) {
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println((Math.random() * 100));
+		
+	}
+}
