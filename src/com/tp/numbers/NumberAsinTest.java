34a0d8be4470c5bc4a6b5ca78e02cb6fff29c7cd
Marlon <martianworm17@gmail.com>
2017-11-28 15:37:16 +0800

    Java - asin() Method

---
+++ src/com/tp/numbers/NumberAsinTest.java
@@ -0,0 +1,13 @@
+package com.tp.numbers;
+
+public class NumberAsinTest {
+ 
+	public static void main(String[] args) {
+		double degrees = 45.0;
+		double radians = Math.toRadians(degrees);
+		
+		System.out.format("The value of pi is %.4f%n", Math.PI);
+		System.out.format("The arcsine of %.4f is %.4f degrees %n", Math.sin(radians), Math.toDegrees(Math.asin(Math.sin(radians))));
+
+	}
+}
