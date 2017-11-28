5f570eab3f756f848dc2df0e5d985e624ef39c82
Marlon <martianworm17@gmail.com>
2017-11-28 15:44:49 +0800

    Java - toDegrees() Method

---
+++ src/com/tp/numbers/NumberToDegreesTest.java
@@ -0,0 +1,13 @@
+package com.tp.numbers;
+
+public class NumberToDegreesTest {
+	
+	public static void main(String[] args) {
+		double x = 45.0;
+		double y = 30.0;
+		
+		System.out.println(Math.toDegrees(x));
+		System.out.println(Math.toDegrees(y));
+	}
+
+}
