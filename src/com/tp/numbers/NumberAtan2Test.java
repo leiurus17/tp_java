00fe257df8e780b2d8d0c57e0e95ca26971c77ef
Marlon <martianworm17@gmail.com>
2017-11-28 15:43:08 +0800

    Java - atan2() Method

---
+++ src/com/tp/numbers/NumberAtan2Test.java
@@ -0,0 +1,11 @@
+package com.tp.numbers;
+
+public class NumberAtan2Test {
+
+	public static void main(String[] args) {
+		double x = 45.0;
+		double y = 30.0;
+		
+		System.out.println(Math.atan2(x, y));
+		}
+}
