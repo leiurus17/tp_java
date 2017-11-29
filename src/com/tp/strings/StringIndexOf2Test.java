e907e17e2477460e845a46021332de4f9ed7e8ce
Marlon <martianworm17@gmail.com>
2017-11-29 19:19:42 +0800

    String indexOf(int ch, int fromIndex)

---
+++ src/com/tp/strings/StringIndexOf2Test.java
@@ -0,0 +1,11 @@
+package com.tp.strings;
+
+public class StringIndexOf2Test {
+	
+	public static void main(String[] args) {
+		String Str = new String("Welcome to Tutorialspoint.com");
+		System.out.print("Found Index: ");
+		System.out.println(Str.indexOf('o', 5));
+	}
+
+}
