6dec282d3b0bce5a3d5b493f8c56c2f805f463f7
Marlon <martianworm17@gmail.com>
2017-11-29 14:37:04 +0800

    Java - isDigit() Method

---
+++ src/com/tp/characters/IsDigitTest.java
@@ -0,0 +1,9 @@
+package com.tp.characters;
+
+public class IsDigitTest {
+
+	public static void main(String[] args) {
+		System.out.println(Character.isDigit('c'));
+		System.out.println(Character.isDigit('5'));
+	}
+}
