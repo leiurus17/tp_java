ccd4684c7a82c8217b6ed6b4031eda875acb6ce8
Marlon <martianworm17@gmail.com>
2017-11-29 17:32:22 +0800

    Java - String equals() Method

---
+++ src/com/tp/strings/StringEqualsTest.java
@@ -0,0 +1,18 @@
+package com.tp.strings;
+
+public class StringEqualsTest {
+
+	public static void main(String[] args) {
+		String Str1 = new String("This is really not immutable!!");
+		String Str2 = Str1;
+		String Str3 = new String("This is really not immutable!!");
+		boolean retVal;
+		
+		retVal = Str1.equals(Str2);
+		System.out.println("Returned Value = " + retVal);
+		
+		retVal = Str1.equals(Str3);
+		System.out.println("Returned Value = " + retVal);
+		
+	}
+}
