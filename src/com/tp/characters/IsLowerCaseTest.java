c825191b44849131ef2acc56b229ebdbdf997ef6
Marlon <martianworm17@gmail.com>
2017-11-29 14:43:04 +0800

    Java - isLowerCase() Method

---
+++ src/com/tp/characters/IsLowerCaseTest.java
@@ -0,0 +1,11 @@
+package com.tp.characters;
+
+public class IsLowerCaseTest {
+
+	public static void main(String[] args) {
+		System.out.println(Character.isLowerCase('c'));
+		System.out.println(Character.isLowerCase('C'));
+		System.out.println(Character.isLowerCase('\n'));
+		System.out.println(Character.isLowerCase('\t'));
+	}
+}
