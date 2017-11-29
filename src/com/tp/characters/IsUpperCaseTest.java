18c4c192292c57c48f937cb3cc0d41f4d615dfcf
Marlon <martianworm17@gmail.com>
2017-11-29 14:42:08 +0800

    Java - isUpperCase() Method

---
+++ src/com/tp/characters/IsUpperCaseTest.java
@@ -0,0 +1,12 @@
+package com.tp.characters;
+
+public class IsUpperCaseTest {
+	
+	public static void main(String[] args) {
+		System.out.println(Character.isUpperCase('c'));
+		System.out.println(Character.isUpperCase('C'));
+		System.out.println(Character.isUpperCase('\n'));
+		System.out.println(Character.isUpperCase('\t'));
+	}
+
+}
