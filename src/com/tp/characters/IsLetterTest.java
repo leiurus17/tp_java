561b21bcb286993f00df64e27b10d493e474a5ed
Marlon <martianworm17@gmail.com>
2017-11-29 14:33:31 +0800

    Java - isLetter() Method

---
+++ src/com/tp/characters/IsLetterTest.java
@@ -0,0 +1,9 @@
+package com.tp.characters;
+
+public class IsLetterTest {
+
+	public static void main(String[] args) {
+		System.out.println(Character.isLetter('c'));
+		System.out.println(Character.isLetter('5'));
+	}
+}
