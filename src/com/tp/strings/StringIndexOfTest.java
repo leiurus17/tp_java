3c0e1d0d1000370c1268927e519fc9c20ee4774b
Marlon <martianworm17@gmail.com>
2017-11-29 19:14:43 +0800

    Java – String indexOf() Method

---
+++ src/com/tp/strings/StringIndexOfTest.java
@@ -0,0 +1,10 @@
+package com.tp.strings;
+
+public class StringIndexOfTest {
+
+	public static void main(String[] args) {
+		String Str = new String("Welcome to Tutorialspoint.com");
+		System.out.print("Found Index: ");
+		System.out.println(Str.indexOf('o'));
+	}
+}
