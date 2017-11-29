30bad45d0ff288db4ebb30feecc139bb235beb96
Marlon <martianworm17@gmail.com>
2017-11-29 15:38:51 +0800

    Java - String charAt() Method

---
+++ src/com/tp/strings/StringCharAtTest.java
@@ -0,0 +1,12 @@
+package com.tp.strings;
+
+public class StringCharAtTest {
+
+	public static void main(String[] args) {
+		String s = "Strings are immutable";
+		
+		char result = s.charAt(8);
+		
+		System.out.println(result);
+	}
+}
