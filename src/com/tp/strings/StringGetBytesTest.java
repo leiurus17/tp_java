60945853f54150bfc2b5fbc2e08bcd35cbc3ba3f
Marlon <martianworm17@gmail.com>
2017-11-29 17:43:39 +0800

    Java – String getBytes() Method

---
+++ src/com/tp/strings/StringGetBytesTest.java
@@ -0,0 +1,21 @@
+package com.tp.strings;
+
+import java.io.UnsupportedEncodingException;
+
+public class StringGetBytesTest {
+
+	public static void main(String[] args) {
+		String Str1 = new String("Welcome to Tutorialspoint.com");
+		
+		try {
+			String Str2 = new String(Str1.getBytes("UTF-8"));
+			System.out.println("Returned Value " + Str2);
+			
+			Str2 = new String(Str1.getBytes("ISO-8859-1"));
+			System.out.println("Returned Value " + Str2);
+		} catch (UnsupportedEncodingException e) {
+			System.out.println("Unsupported character set");
+		}
+	}
+	
+}
