7245f9e722200ffa4408dc2a422b0564ebcf4edf
Marlon <martianworm17@gmail.com>
2017-11-29 14:44:07 +0800

    Java - toUpperCase() Method

---
+++ src/com/tp/characters/ToUpperCaseTest.java
@@ -0,0 +1,10 @@
+package com.tp.characters;
+
+public class ToUpperCaseTest {
+	
+	public static void main(String[] args) {
+		System.out.println(Character.toUpperCase('c'));
+		System.out.println(Character.toUpperCase('C'));
+	}
+
+}
