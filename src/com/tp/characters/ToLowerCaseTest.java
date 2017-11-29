95b8e642f630aac4388edbb45076330b45cd9fe0
Marlon <martianworm17@gmail.com>
2017-11-29 14:44:56 +0800

    Java - toLowerCase() Method

---
+++ src/com/tp/characters/ToLowerCaseTest.java
@@ -0,0 +1,9 @@
+package com.tp.characters;
+
+public class ToLowerCaseTest {
+
+	public static void main(String[] args) {
+		System.out.println(Character.toLowerCase('c'));
+		System.out.println(Character.toLowerCase('C'));
+	}
+}
