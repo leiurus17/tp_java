73754a189125f290482d1ea3e186e28f0ec4afba
Marlon <martianworm17@gmail.com>
2017-11-29 19:01:09 +0800

    Java - String hashCode() Method

---
+++ src/com/tp/strings/StringHashCodeTest.java
@@ -0,0 +1,10 @@
+package com.tp.strings;
+
+public class StringHashCodeTest {
+	
+	public static void main(String[] args) {
+		String Str = new String("Welcome to Tutorialspoint.com");
+		System.out.println("Hashcode for Str: " + Str.hashCode());
+	}
+
+}
