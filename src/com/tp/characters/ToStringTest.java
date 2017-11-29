e42789125c143f8dc77d54d39719e0b2e68a6d49
Marlon <martianworm17@gmail.com>
2017-11-29 14:45:47 +0800

    Java - toString() Method

---
+++ src/com/tp/characters/ToStringTest.java
@@ -0,0 +1,10 @@
+package com.tp.characters;
+
+public class ToStringTest {
+	
+	public static void main(String[] args) {
+		System.out.println(Character.toString('c'));
+		System.out.println(Character.toString('C'));
+	}
+
+}
