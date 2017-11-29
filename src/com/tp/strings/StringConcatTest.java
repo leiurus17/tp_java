01f5799177ff3d5e315776a659c327015db8ed0d
Marlon <martianworm17@gmail.com>
2017-11-29 15:56:18 +0800

    Java - String concat() Method

---
+++ src/com/tp/strings/StringConcatTest.java
@@ -0,0 +1,11 @@
+package com.tp.strings;
+
+public class StringConcatTest {
+
+	public static void main(String args[]) {
+		String s = "Strings are immutable";
+		s = s.concat(" all the time");
+		
+		System.out.println(s);
+	}
+}
