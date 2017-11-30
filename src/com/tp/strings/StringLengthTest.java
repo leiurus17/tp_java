01c04b6a3e941583221b5e34055b930a5c791443
Marlon <martianworm17@gmail.com>
2017-11-30 15:49:06 +0800

    Java - String length() Method

---
+++ src/com/tp/strings/StringLengthTest.java
@@ -0,0 +1,15 @@
+package com.tp.strings;
+
+public class StringLengthTest {
+
+	public static void main(String[] args) {
+		String str1 = new String("Welcome to the Black Parade!");
+		String str2 = new String("Black Parade");
+		
+		System.out.print("String Length: ");
+		System.out.println(str1.length());
+		
+		System.out.print("String Length: ");
+		System.out.println(str2.length());
+	}
+}
