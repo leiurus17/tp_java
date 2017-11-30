f5fd41e726104da30364ac137178ca1416c90605
Marlon <martianworm17@gmail.com>
2017-11-30 15:39:29 +0800

    String lastIndexOf(int ch, int fromIndex)

---
+++ src/com/tp/strings/StringLastIndexOf2Test.java
@@ -0,0 +1,11 @@
+package com.tp.strings;
+
+public class StringLastIndexOf2Test {
+	
+	public static void main(String[] args) {
+		String str = new String("Welcome to the Black Parade!");
+		System.out.println("Found Last Index: ");
+		System.out.println(str.lastIndexOf('o', 5));
+	}
+
+}
