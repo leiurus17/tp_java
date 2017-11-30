9c1ec8dab24daaba475a57d7372f4ca8a52cf7e9
Marlon <martianworm17@gmail.com>
2017-11-30 15:36:42 +0800

    Java – String lastIndexOf() Method

---
+++ src/com/tp/strings/StringLastIndexOfTest.java
@@ -0,0 +1,12 @@
+package com.tp.strings;
+
+public class StringLastIndexOfTest {
+
+	public static void main(String[] args) {
+		String str = new String("Welcome to the Black Parade!");
+		System.out.print("Found Last Index: ");
+		System.out.println(str.lastIndexOf('o'));
+		System.out.print("Found First Index: ");
+		System.out.println(str.indexOf('o'));
+	}
+}
