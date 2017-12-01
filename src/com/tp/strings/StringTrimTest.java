d62b824e52cc4dfb86f787ef7fb86addc046c95b
Marlon <martianworm17@gmail.com>
2017-12-01 11:04:20 +0800

    Java - String trim() Method

---
+++ src/com/tp/strings/StringTrimTest.java
@@ -0,0 +1,11 @@
+package com.tp.strings;
+
+public class StringTrimTest {
+
+	public static void main(String[] args) {
+		String str = new String("         Welcome to the Black      Parade                     ");
+		
+		System.out.print("Return value: ");
+		System.out.println(str.trim());
+	}
+}
