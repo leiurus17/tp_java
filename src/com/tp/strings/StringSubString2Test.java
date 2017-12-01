620da17bbac48e2da542cd76cf1c170c3dd84b0b
Marlon <martianworm17@gmail.com>
2017-12-01 10:18:50 +0800

    String substring(beginIndex, endIndex)

---
+++ src/com/tp/strings/StringSubString2Test.java
@@ -0,0 +1,11 @@
+package com.tp.strings;
+
+public class StringSubString2Test {
+
+	public static void main(String[] args) {
+		String str = new String("Welcome to the Black Parade");
+		
+		System.out.print("Return value: ");
+		System.out.println(str.substring(15, 20));
+	}
+}
