90d9d719a3a1306346c5823bd0ccb24f4a3a7767
Marlon <martianworm17@gmail.com>
2017-12-01 10:53:19 +0800

    Java - String toUpperCase() Method

---
+++ src/com/tp/strings/StringToUpperCaseTest.java
@@ -0,0 +1,11 @@
+package com.tp.strings;
+
+public class StringToUpperCaseTest {
+
+	public static void main(String[] args) {
+		String str = new String("Welcome to the Black Parade");
+		
+		System.out.print("Return value: ");
+		System.out.println(str.toUpperCase());
+	}
+}
