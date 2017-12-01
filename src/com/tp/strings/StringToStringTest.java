f6d310fc70c8b255df5ee84f1c41097c50e65539
Marlon <martianworm17@gmail.com>
2017-12-01 10:36:10 +0800

    Java - String toString() Method

---
+++ src/com/tp/strings/StringToStringTest.java
@@ -0,0 +1,14 @@
+package com.tp.strings;
+
+public class StringToStringTest {
+
+	public static void main(String[] args) {
+		Integer numVal        = 234354;
+		Integer anotherNumVal = 111111;
+		String str = "Welcome to the Black Parade";
+		
+		System.out.println(numVal + anotherNumVal);
+		System.out.println(numVal.toString() + anotherNumVal.toString());
+		System.out.println(str.toString());
+	}
+}
