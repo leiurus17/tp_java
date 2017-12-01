82e4a8e56c4809fc4c714f800f7247d16b6700ac
Marlon <martianworm17@gmail.com>
2017-12-01 11:40:07 +0800

    Java - Arrays - Processing Arrays

---
+++ src/com/tp/arrays/TestArray1.java
@@ -0,0 +1,29 @@
+package com.tp.arrays;
+
+public class TestArray1 {
+	
+	public static void main(String[] args) {
+		double[] myList = {1.9, 2.9, 3.4, 3.5};
+		
+		// Print all the array elements
+		for (int i = 0; i < myList.length; i++) {
+			System.out.println(myList[i] + " ");
+		}
+		
+		// Summing all elements
+		double total = 0;
+		
+		for (int i = 0; i < myList.length; i++) {
+			total += myList[i];
+		}
+		
+		// Finding the largest element
+		double max = myList[0];
+		
+		for (int i = 1; i < myList.length; i++) {
+			if (myList[i] > max) max = myList[i];
+		}
+		System.out.println("Max value is: " + max);
+	}
+
+}
