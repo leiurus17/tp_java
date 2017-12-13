e77ad9ac941d7074230d6250412953fb32d4b01a
Marlon <martianworm17@gmail.com>
2017-12-13 22:05:49 +0800

    Getting Current Date and Time

---
+++ src/com/tp/datetime/DateDemo.java
@@ -0,0 +1,15 @@
+package com.tp.datetime;
+
+import java.util.Date;
+
+public class DateDemo {
+	
+	public static void main(String[] args) {
+		// Instantiate a Date object
+		Date date = new Date();
+		
+		// Display time and date using toString()
+		System.out.println(date.toString());
+	}
+
+}
