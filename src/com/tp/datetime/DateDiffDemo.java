6ee6f4e001bdc8ed797c00aab995e3acc5362594
Marlon <martianworm17@gmail.com>
2017-12-16 12:15:03 +0800

    Measuring Elapsed Time

---
+++ src/com/tp/datetime/DateDiffDemo.java
@@ -0,0 +1,23 @@
+package com.tp.datetime;
+
+import java.util.Date;
+
+public class DateDiffDemo {
+
+	public static void main(String[] args) {
+		try {
+			long start = System.currentTimeMillis();
+			System.out.println(new Date() + "\n");
+			
+			Thread.sleep(5 * 60 * 10);
+			System.out.println(new Date() + "\n");
+			
+			long end  = System.currentTimeMillis();
+			long diff = end - start;
+			
+			System.out.println("Difference is : " + diff);
+		} catch (Exception e) {
+			System.out.println("Got an exception!");
+		}
+	}
+}
