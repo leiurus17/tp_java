11747ef3050a26e67aa0419add16f6f492cacdc9
Marlon <martianworm17@gmail.com>
2017-12-17 17:24:39 +0800

    The appendReplacement and appendTail Methods

---
+++ src/com/tp/regularexpressions/RegexMatchesAppend.java
@@ -0,0 +1,29 @@
+package com.tp.regularexpressions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexMatchesAppend {
+	
+	private static String REGEX   = "a*b";
+	private static String INPUT   = "aabfooaabfooabfoob";
+	private static String REPLACE = "-";
+	
+	public static void main(String[] args) {
+		
+		Pattern p = Pattern.compile(REGEX);
+		
+		// Get a matcher object
+		Matcher m = p.matcher(INPUT);
+		StringBuffer sb = new StringBuffer();
+		
+		while(m.find()) {
+			m.appendReplacement(sb, REPLACE);
+		}
+		
+		m.appendTail(sb);
+		
+		System.out.println(sb.toString());
+	}
+
+}
