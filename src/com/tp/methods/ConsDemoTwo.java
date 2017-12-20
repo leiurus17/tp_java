253890bba50b85223f20ee66fee9cb076e489996
Marlon <martianworm17@gmail.com>
2017-12-20 22:36:24 +0800

    Parameterized Constructor

---
+++ src/com/tp/methods/ConsDemoTwo.java
@@ -0,0 +1,11 @@
+package com.tp.methods;
+
+public class ConsDemoTwo {
+
+	public static void main(String[] args) {
+		MyClassTwo t1 = new MyClassTwo(10);
+		MyClassTwo t2 = new MyClassTwo(20);
+		
+		System.out.println(t1.x + " " + t2.x);
+	}
+}
---
+++ src/com/tp/methods/MyClassTwo.java
@@ -0,0 +1,12 @@
+package com.tp.methods;
+
+class MyClassTwo {
+
+	int x;
+	
+	// Following is the constructor
+	MyClassTwo(int i) {
+		x = i;
+	}
+
+}
