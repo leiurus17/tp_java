744f1c72cf863e6f7fee86a122bf0d5c9becb420
Marlon <martianworm17@gmail.com>
2017-12-26 22:27:16 +0800

    Java - Exceptions - User-defined Exceptions

---
+++ src/com/tp/exceptions/InsufficientFundsException.java
@@ -0,0 +1,14 @@
+package com.tp.exceptions;
+
+public class InsufficientFundsException extends Exception {
+
+	private double amount;
+	
+	public InsufficientFundsException(double amount) {
+		this.amount = amount;
+	}
+	
+	public double getAmount() {
+		return amount;
+	}
+}
