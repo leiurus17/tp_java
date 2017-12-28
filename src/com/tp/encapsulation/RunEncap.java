22c82252423d7ee04d654b329531a84c5f72f88b
Marlon <martianworm17@gmail.com>
2017-12-29 00:12:37 +0800

    Java - Encapsulation

---
+++ src/com/tp/encapsulation/RunEncap.java
@@ -0,0 +1,15 @@
+package com.tp.encapsulation;
+
+public class RunEncap {
+
+	public static void main(String[] args) {
+		EncapTest encap = new EncapTest();
+		
+		encap.setName("James");
+		encap.setAge(20);
+		encap.setIdNum("12343ms");
+		
+		System.out.println("Name: " + encap.getName() + " Age: " + encap.getAge());
+	}
+	
+}
