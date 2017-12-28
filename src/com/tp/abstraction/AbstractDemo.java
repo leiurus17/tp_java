aa4cccf580b79b5b1635dc59739ddd363a3e3f7e
Marlon <martianworm17@gmail.com>
2017-12-28 23:31:41 +0800

    Java - Abstraction

---
+++ src/com/tp/abstraction/AbstractDemo.java
@@ -0,0 +1,16 @@
+package com.tp.abstraction;
+
+public class AbstractDemo {
+	
+	public static void main(String[] args) {
+		/* Following is not allow and would raise error */
+		
+		//Employee e = new Employee("Naruto", "Konoha", 43);
+     	
+		/* Cannot instantiate the type Employee */
+		
+		System.out.println("Call mailCheck using Employee reference.");
+		//e.mailCheck();
+	}
+
+}
