c6190ac99ba7fab3708d76f441f99e845f1b364e
Marlon <martianworm17@gmail.com>
2017-12-28 20:43:29 +0800

    Java - Overriding

---
+++ src/com/tp/overriding/Animal.java
@@ -0,0 +1,9 @@
+package com.tp.overriding;
+
+public class Animal {
+	
+	public void move() {
+		System.out.println("Animals can move.");
+	}
+
+}
---
+++ src/com/tp/overriding/Dog.java
@@ -0,0 +1,7 @@
+package com.tp.overriding;
+
+public class Dog extends Animal {
+	public void move() {
+		System.out.println("Dogs can walk and run.");
+	}
+}
