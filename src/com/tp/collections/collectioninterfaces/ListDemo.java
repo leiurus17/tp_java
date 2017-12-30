b00429cb092918f2b0ec4d48dc059476081f0eea
Marlon <martianworm17@gmail.com>
2017-12-30 21:51:42 +0800

    Java - The List Interface

--- src/com/tp/collections/collectioninterfaces/ListDemo.java
+++ src/com/tp/collections/collectioninterfaces/ListDemo.java
@@ -10,10 +10,18 @@ public class ListDemo {
 		a1.add("Zara");
 		a1.add("Mahnaz");
 		a1.add("Ayan");
+		// Duplicate items allowed
+		a1.add("Zara");
+		a1.add("Mahnaz");
+		a1.add("Ayan");
 		System.out.println(" ArrayList Elements");
 		System.out.print("\t" + a1);
 		
 		List l1 = new LinkedList();
+		l1.add("Zara");
+		l1.add("Mahnaz");
+		l1.add("Ayan");
+		// Duplicate items allowed
 		l1.add("Zara");
 		l1.add("Mahnaz");
 		l1.add("Ayan");
