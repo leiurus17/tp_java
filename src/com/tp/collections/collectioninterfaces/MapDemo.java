7b81f4c7acea66dc0dc371798b36db473a39acd5
Marlon <martianworm17@gmail.com>
2017-12-31 17:01:49 +0800

    Java - The Map Interface

---
+++ src/com/tp/collections/collectioninterfaces/MapDemo.java
@@ -0,0 +1,21 @@
+package com.tp.collections.collectioninterfaces;
+
+import java.util.*;
+
+public class MapDemo {
+
+	public static void main(String[] args) {
+		
+		Map m1 = new HashMap();
+		m1.put("Zara", "8");
+		m1.put("Mahnaz", "31");
+		m1.put("Ayan", "12");
+		m1.put("Daisy", "14");
+		
+		System.out.println();
+		System.out.println(" Map Elements");
+		System.out.println("\t" + m1);
+		
+	}
+	
+}
