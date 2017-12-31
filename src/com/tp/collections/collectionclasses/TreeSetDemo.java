651f25be0d087380cc0ca1149414c96860a2d3b4
Marlon <martianworm17@gmail.com>
2017-12-31 22:26:05 +0800

    Java - The TreeSet Class

---
+++ src/com/tp/collections/collectionclasses/TreeSetDemo.java
@@ -0,0 +1,21 @@
+package com.tp.collections.collectionclasses;
+
+import java.util.*;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		
+		// Create a tree set
+		TreeSet ts = new TreeSet();
+		
+		// Add elements to the tree set
+		ts.add("C");
+		ts.add("A");
+		ts.add("B");
+		ts.add("E");
+		ts.add("F");
+		ts.add("D");
+		System.out.println(ts);
+	}
+}
