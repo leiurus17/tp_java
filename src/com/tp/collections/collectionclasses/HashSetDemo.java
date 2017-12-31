506cb11e1e3472fcaa85579fadcddb1cae83e45e
Marlon <martianworm17@gmail.com>
2017-12-31 21:36:41 +0800

    Java - The HashSet Class

---
+++ src/com/tp/collections/collectionclasses/HashSetDemo.java
@@ -0,0 +1,21 @@
+package com.tp.collections.collectionclasses;
+
+import java.util.*;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		
+		// Create a hash set
+		HashSet hs = new HashSet();
+		
+		// Add elements to the hash set
+		hs.add("B");
+		hs.add("A");
+		hs.add("D");
+		hs.add("E");
+		hs.add("C");
+		hs.add("F");
+		System.out.println(hs);
+	}
+}
