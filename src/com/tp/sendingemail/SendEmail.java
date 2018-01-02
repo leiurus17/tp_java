97c9313345d06412a81e00c6bf6cee1a0f371441
Marlon <martianworm17@gmail.com>
2018-01-02 22:24:44 +0800

    Java - Sending Email

--- src/com/tp/sendingemail/SendEmail.java
+++ src/com/tp/sendingemail/SendEmail.java
@@ -7,4 +7,29 @@ import javax.activation.*;
 
 public class SendEmail {
 
+	public static void main(String[] args) {
+		
+		String to = "abcd@gmail.com";
+		String from = "web@gmail.com";
+		String host = "localhost";
+		Properties properties = System.getProperties();
+		properties.setProperty("mail.smtp.host", host);
+		
+		Session session = Session.getDefaultInstance(properties);
+		
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+			message.setSubject("This is the Subject Line!");
+			message.setText("This is the actual message.");
+			
+			// Send message
+			Transport.send(message);
+			System.out.println("Sent message successfully...");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+		
+	}
 }
