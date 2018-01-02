fe5109a6013b18153b04632635d5f73f171ae6ce
Marlon <martianworm17@gmail.com>
2018-01-02 15:03:22 +0800

    Java - Serialization

---
+++ src/com/tp/serialization/Employee.java
@@ -0,0 +1,14 @@
+package com.tp.serialization;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+	public String name;
+	public String address;
+	public transient int SSN;
+	public int number;
+	
+	public void mailCheck() {
+		System.out.println("Mailing a check to " + name + " " + address);
+	}
+}
