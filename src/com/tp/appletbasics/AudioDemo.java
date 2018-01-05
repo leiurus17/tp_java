7d5861905a4c1c93d249c9f9ec62acf99ca05fd8
Marlon Monzon <martianworm17@gmail.com>
2018-01-05 19:30:53 +0800

    Java - Applet Basics - Prepare sources

---
+++ src/com/tp/appletbasics/AudioDemo.java
@@ -0,0 +1,5 @@
+package com.tp.appletbasics;
+
+public class AudioDemo {
+
+}
---
+++ src/com/tp/appletbasics/ExampleEventHandling.java
@@ -0,0 +1,39 @@
+package com.tp.appletbasics;
+
+import java.applet.Applet;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class ExampleEventHandling extends Applet implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
