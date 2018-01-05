7cd34d81a6c5f097170e9097fd04d370284b7614
Marlon Monzon <martianworm17@gmail.com>
2018-01-05 18:53:15 +0800

    Java - Applet Basics - A "Hello, World" Applet

---
+++ src/com/tp/appletbasics/HelloWorldApplet.java
@@ -0,0 +1,11 @@
+package com.tp.appletbasics;
+
+import java.applet.*;
+import java.awt.*;
+
+public class HelloWorldApplet extends Applet {
+
+	public void paint (Graphics g) {
+		g.drawString("Hello World", 25, 50);
+	}
+}
