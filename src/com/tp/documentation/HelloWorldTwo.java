f732d256473eafb8140525425818565899c52ff2
Marlon <martianworm17@gmail.com>
2018-01-07 13:39:39 +0800

    Java - Documentation Comments - Generate JavaDoc

---
+++ src/com/tp/documentation/HelloWorldTwo.java
@@ -0,0 +1,22 @@
+package com.tp.documentation;
+
+/**
+ * <h1>Hello, World!</h1>
+ * The HelloWorldTwo program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ * <p>
+ * Giving proper comments in your program makes it more
+ * user friendly and it is assumed as a high quality code.
+ * 
+ * 
+ * @author Lon
+ * @version 1.0
+ * @since 2018-07-01
+ */
+public class HelloWorldTwo {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
