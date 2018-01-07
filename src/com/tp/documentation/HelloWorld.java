c59718822716592dfabf70b586f05b4beec81652
Marlon <martianworm17@gmail.com>
2018-01-07 13:30:38 +0800

    Java - Documentation Comments

---
+++ src/com/tp/documentation/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.tp.documentation;
+
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ * 
+ * @author Lon
+ * @version 1.0
+ * @since 2018-07-01
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		/* Prints Hello World! on standard output. */
+		System.out.println("Hello World!");
+	}
+}
